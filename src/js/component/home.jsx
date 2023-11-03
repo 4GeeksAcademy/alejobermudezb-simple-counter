75b240fdc045798ae08a7b6ae9e2110657b1b8ee
Luis Alejandro Bermudez <142993857+Alejobermudezb@users.noreply.github.com>
2023-11-03 11:22:08 +0000

    feat: primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+const Home = (num) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="mainBox">
+			<div className="box"><i className="fa-light fa-clock"></i></div>
+			<div className="box">{num.six}<h1></h1></div>
+			<div className="box">{num.five}</div>
+			<div className="box">{num.four}</div>
+			<div className="box">{num.three}</div>
+			<div className="box">{num.two}</div>
+			<div className="box">{num.one}</div>
 		</div>
+		
+		
 	);
 };
 
