6c921f5097ca0ea8bf17d2677a97206ee43d8290
Luis Alejandro Bermudez <142993857+Alejobermudezb@users.noreply.github.com>
2023-11-03 19:01:41 +0000

    commit sin bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,11 +7,14 @@ const Home = (num) => {
 	return (
 		<div id="mainBox">
 			<div className="box"><i className={"fa fa-clock"}></i></div>
-			<div className="box">{num.six}<h1></h1></div>
+			<div className="box">{num.eight}</div>
+			<div className="box">{num.seven}</div>
+			<div className="box">{num.six}</div>
 			<div className="box">{num.five}</div>
 			<div className="box">{num.four}</div>
 			<div className="box">{num.three}</div>
 			<div className="box">{num.two}</div>
+			<div>,</div>
 			<div className="box">{num.one}</div>
 		</div>
 		
--- src/js/index.js
+++ src/js/index.js
@@ -12,12 +12,14 @@ import Home from "./component/home.jsx";
 let counter = 0;
 setInterval(() => {
     ReactDOM.render(<Home 
-        one = {Math.floor((counter/1)%10)}
+        one = {Math.floor((counter)%10)}
         two = {Math.floor((counter/10)%10)}
         three = {Math.floor((counter/100)%10)}
         four = {Math.floor((counter/1000)%10)}
         five = {Math.floor((counter/10000)%10)}
         six = {Math.floor((counter/100000)%10)}
+        seven = {Math.floor((counter/1000000)%10)}
+        eight = {Math.floor((counter/10000000)%10)}
         />, document.querySelector("#app"));
         counter++
     
